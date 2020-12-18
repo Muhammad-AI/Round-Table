88b3216bf3509efadfcb5350332c7aff1617447d
Muhammad Badami <z5259293@ad.unsw.edu.au>
2020-12-18 14:31:00 +1100

    Added Firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Button from '@material-ui/core/Button';
 
 const App = () => {
   return (
     <div>
-      Hello World
+      <Button variant="contained" color="primary">
+        JOIN
+      </Button>
+      <Button variant="contained" color="secondary">
+        HOST
+      </Button>
     </div>
   )
 }
