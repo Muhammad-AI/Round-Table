18d49f821e920c002b4597cbc93e3a8988c4272b
Muhammad-AI <55338582+Muhammad-AI@users.noreply.github.com>
2021-11-23 22:30:55 +1100

    Store file added

---
+++ src/store.js
@@ -0,0 +1,7 @@
+import {craeteStore, combineReducers, createStore } from 'redux'
+
+const reducers = {}
+
+const rootReducer = combineReducers(reducers)
+
+export const configureStore = () => createStore(rootReducer)
